7b878ff7afe20646baca0b59be154d2eadeadfbe
Kevin McCafferty <kevin.mccafferty@ons.gov.uk>
2024-02-12 17:32:47 +0000

    Loading docking stations into dataframe

--- bikes_cleaning.R
+++ bikes_cleaning.R
@@ -1,1 +1,11 @@
-# Get all points from API
+# Load libraries
+library(tidyverse)
+library(httr)
+
+# Get docking stations from API
+bikes = GET("https://api.tfl.gov.uk/BikePoint/")
+bikes_result = content(bikes)
+latitudes = do.call("rbind", lapply(bikes_result, "[[", "lat"))
+longitudes = do.call("rbind", lapply(bikes_result, "[[", "lon"))
+station_names = do.call("rbind", lapply(bikes_result, "[[", "commonName"))
+stations = data.frame(latitude=latitudes, longitude=longitudes, name=station_names)
